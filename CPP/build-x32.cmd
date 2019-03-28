051777665cf1f7e190b1bbf41043f70e2d7660d2
conor42 <ronoc74@yahoo.com>
2019-03-28 23:58:11 +1000

    Change x86 source dir

--- CPP/build-x32.cmd
+++ CPP/build-x32.cmd
@@ -9,40 +9,40 @@ set LFLAGS=/SUBSYSTEM:WINDOWS,"5.01"
 
 cd %ROOT%\Bundles\Format7z
 nmake %OPTS%
-copy O\7za.dll %OUTDIR%\7za.dll
+copy x86\7za.dll %OUTDIR%\7za.dll
 
 cd %ROOT%\Bundles\Format7zF
 nmake %OPTS%
-copy O\7z.dll %OUTDIR%\7z.dll
+copy x86\7z.dll %OUTDIR%\7z.dll
 
 cd %ROOT%\UI\FileManager
 nmake %OPTS%
-copy O\7zFM.exe %OUTDIR%\7zFM.exe
+copy x86\7zFM.exe %OUTDIR%\7zFM.exe
 
 cd %ROOT%\UI\GUI
 nmake %OPTS%
-copy O\7zG.exe %OUTDIR%\7zG.exe
+copy x86\7zG.exe %OUTDIR%\7zG.exe
 
 cd %ROOT%\Bundles\Codec_flzma2
 nmake %OPTS%
-copy O\flzma2.dll %OUTDIR%\flzma2-x32.dll
+copy x86\flzma2.dll %OUTDIR%\flzma2-x32.dll
 
 cd %ROOT%\..\..\C\Util\7zipInstall
 nmake %OPTS%
-copy O\7zipInstall.exe %OUTDIR%\Install-x32.exe
+copy x86\7zipInstall.exe %OUTDIR%\Install-x32.exe
 
 cd %ROOT%\..\..\C\Util\7zipUninstall
 nmake %OPTS%
-copy O\7zipUninstall.exe %OUTDIR%\Uninstall.exe
+copy x86\7zipUninstall.exe %OUTDIR%\Uninstall.exe
 
 set LFLAGS=/SUBSYSTEM:CONSOLE,"5.01"
 cd %ROOT%\UI\Console
 nmake %OPTS%
-copy O\7z.exe %OUTDIR%\7z.exe
+copy x86\7z.exe %OUTDIR%\7z.exe
 
 cd %ROOT%\Bundles\Alone
 nmake %OPTS%
-copy O\7za.exe %OUTDIR%\7za.exe
+copy x86\7za.exe %OUTDIR%\7za.exe
 
 :ende
 cd %ROOT%\..
