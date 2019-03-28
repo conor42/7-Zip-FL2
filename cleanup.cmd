d8d08c172c47e89ed8cf87e723fc7efabaaa06c3
conor42 <ronoc74@yahoo.com>
2019-03-28 23:59:43 +1000

    Copy cleanup script from 7-Zip-zstd

---
+++ cleanup.cmd
@@ -0,0 +1,10 @@
+@echo off
+
+FOR /R .\ %%d IN (x64 x86) DO rd /S /Q %%d 2>NUL
+
+del "CPP\7zip\*.7z" 2>NUL
+del "CPP\7zip\*.exe" 2>NUL
+rd /S /Q "CPP\7zip\bin*" 2>NUL
+rd /S /Q "CPP\7zip\codecs" 2>NUL
+rd /S /Q "CPP\7zip\totalcmd" 2>NUL
+
